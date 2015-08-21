fe6abfafe53f364ccb6da263539b315145ab63e6
Tim Freiheit <timfreiheit@hotmail.de>
2015-08-21 10:53:55 +0200

    add back navigation and selected state to sidemenu

--- Wheelmap-Windows/Source/UI/MainPage.xaml.cs
+++ Wheelmap-Windows/Source/UI/MainPage.xaml.cs
@@ -22,24 +22,71 @@ using Wheelmap_Windows.Utils.Eventbus;
 using Wheelmap_Windows.Utils.Eventbus.Events;
 using Wheelmap_Windows.Source.UI.Pages.Node;
 using Windows.UI;
+using Windows.UI.Core;
+using Wheelmap_Windows.Utils;
 
 namespace Wheelmap_Windows.Source.UI {
     
     public sealed partial class MainPage : Page {
 
+        ToggleGroup<Panel> mToggleGroup;
+
         public MainPage() {
 
             this.InitializeComponent();
+            InitToggleGroup();
+
             mainFrame.Navigate(typeof(MapPage));
             //var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
             
             BusProvider.DefaultInstance.Register(this);
+
+            SystemNavigationManager.GetForCurrentView().BackRequested += (s, e) => {
+            if (detailContainerFrame.Content != null) {
+                    detailContainerFrame.Content = null;
+                    SetBackButtonStatus();
+                    return;
+                }
+                if (menuContainerFrame.Content != null) {
+                    menuContainerFrame.Content = null;
+                    SetBackButtonStatus();
+                    return;
+                }
+                SetBackButtonStatus();
+            };
+
+            SetBackButtonStatus();
         }
 
-        private void MenuButton_Click(object sender, RoutedEventArgs e) {
-            toggleMenu();
+        private void InitToggleGroup() {
+            mToggleGroup = new ToggleGroup<Panel>();
+            foreach (Panel p in menuTopPanel.Children) {
+                mToggleGroup.Items.Add(p);
+            }
+            foreach (Panel p in menuBottomPanel.Children) {
+                mToggleGroup.Items.Add(p);
+            }
+            mToggleGroup.Delegate = (item, selected) => {
+                if (item == null) {
+                    return;
+                }
+                if (selected) {
+                    item.Background = new SolidColorBrush(Color.FromArgb(0xff, 0x99, 0xc1, 0x5c));
+                } else {
+                    item.Background = new SolidColorBrush(Color.FromArgb(0x00, 0x00, 0x00, 0x00));
+                }
+            };
         }
 
+        private void SetBackButtonStatus() {
+            if (menuContainerFrame.Content != null || detailContainerFrame.Content != null) {
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            } else {
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+                mToggleGroup.SelectedItem = null;
+            }
+        }
+        
         private void ShowMenu(bool show) {
             mSplitView.IsPaneOpen = show;
         }
@@ -49,10 +96,8 @@ namespace Wheelmap_Windows.Source.UI {
             ShowMenu(!mSplitView.IsPaneOpen);
         }
         
-        private void Button_Click(object sender, RoutedEventArgs e) {
-            Debug.WriteLine("Button Clicked");
-            menuContainerFrame.Navigate(typeof(MenuPage));
-            menuContainerFrame.Visibility = Visibility.Visible;
+        private void MenuButton_Click(object sender, RoutedEventArgs e) {
+            toggleMenu();
         }
 
         private void ShowListTapped(object sender, TappedRoutedEventArgs e) {
@@ -65,16 +110,50 @@ namespace Wheelmap_Windows.Source.UI {
                 // remove content
                 (menuContainerFrame.Content as NodeListPage).Unregister();
                 menuContainerFrame.Content = null;
+                SetBackButtonStatus();
                 return;
             }
             
             menuContainerFrame.Navigate(typeof(NodeListPage));
+            SetBackButtonStatus();
+            mToggleGroup.SelectedItem = (sender as Panel);
 
         }
 
+        private void ShowHelpTapped(object sender, TappedRoutedEventArgs e) {
+            ShowMenu(false);
+            Debug.WriteLine("ShowHelpTapped Clicked");
+        }
+        
+        private void ShowWCTapped(object sender, TappedRoutedEventArgs e) {
+            ShowMenu(false);
+            Debug.WriteLine("ShowWCTapped Clicked");
+        }
+
+        private void ShowStatusTapped(object sender, TappedRoutedEventArgs e) {
+            ShowMenu(false);
+            Debug.WriteLine("ShowStatusTapped Clicked");
+        }
+
+        private void ShowCategoryTapped(object sender, TappedRoutedEventArgs e) {
+            ShowMenu(false);
+            Debug.WriteLine("ShowCategoryTapped Clicked");
+        }
+
+        private void ShowProfileTapped(object sender, TappedRoutedEventArgs e) {
+            ShowMenu(false);
+            Debug.WriteLine("ShowProfileTapped Clicked");
+        }
+
+        private void ShowSettingsTapped(object sender, TappedRoutedEventArgs e) {
+            ShowMenu(false);
+            Debug.WriteLine("ShowSettingsTapped Clicked");
+        }
+        
         [Subscribe]
         public void OnSelectedNodeChanged(SelectedNodeChangedEvent e) {
             detailContainerFrame.Navigate(typeof(NodeDetailPage), e.node);
+            SetBackButtonStatus();
         }
 
     }
---
+++ Wheelmap-Windows/Source/Utils/ToggleGroup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wheelmap_Windows.Utils {
+
+    /**
+     * manages a collection of items and their selected state
+     * uses delegate to change the state
+     */
+    public class ToggleGroup<T> {
+
+        public delegate void ChangeState(T item, bool selected);
+
+        private T _SelectedItem;
+        public T SelectedItem {
+            set {
+                Delegate?.Invoke(_SelectedItem, false);
+                _SelectedItem = value;
+                Delegate?.Invoke(_SelectedItem, true);
+            }
+            get {
+                return _SelectedItem;
+            }
+        }
+
+        public List<T> Items = new List<T>();
+
+        public ChangeState Delegate;
+        
+    }
+}
