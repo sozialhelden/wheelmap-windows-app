88df2b7dae2fa8639d6237436a001cc781b6ad9f
Tim Freiheit <timfreiheit@hotmail.de>
2015-09-28 12:03:40 +0200

    test tile notifications for every size

--- Wheelmap.Shared/Source/Tiles/WheelmapTileManager.cs
+++ Wheelmap.Shared/Source/Tiles/WheelmapTileManager.cs
@@ -15,16 +15,26 @@ namespace Wheelmap.Tiles {
             updater.EnableNotificationQueue(true);
             updater.Clear();
             
-            var tileContent = TileContentFactory.CreateTileWide310x150Image();
-            tileContent.Image.Src = "ms-appx:///Assets/App/wide_310x150/wide.png";
-            tileContent.RequireSquare150x150Content = false;
-            updater.Update(tileContent.CreateNotification());
+            updater.Update(createNotification());
+            updater.Update(createNotification());
+            updater.Update(createNotification());
 
-            tileContent = TileContentFactory.CreateTileWide310x150Image();
-            tileContent.Image.Src = "ms-appx:///Assets/App/wide_310x150/wide.png";
-            tileContent.RequireSquare150x150Content = false;
-            updater.Update(tileContent.CreateNotification());
+        }
 
+        private static TileNotification createNotification() {
+
+            var square150x150 = TileContentFactory.CreateTileSquare150x150Image();
+            square150x150.Image.Src = "ms-appx:///Assets/App/square_150x150/square.png";
+
+            var wide310x150 = TileContentFactory.CreateTileWide310x150Image();
+            wide310x150.Image.Src = "ms-appx:///Assets/App/wide_310x150/wide.png";
+            wide310x150.Square150x150Content = square150x150;
+
+            var tileContent = TileContentFactory.CreateTileSquare310x310Image();
+            tileContent.Image.Src = "ms-appx:///Assets/App/square_150x150/square.png";
+            tileContent.Wide310x150Content = wide310x150;
+
+            return tileContent.CreateNotification();
         }
     }
 }
