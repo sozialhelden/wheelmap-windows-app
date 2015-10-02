26c1b021281c908c6aae9c13bd4fd315e51fff9f
Tim Freiheit <timfreiheit@hotmail.de>
2015-10-02 15:50:41 +0200

    add new tiles

--- Wheelmap.Shared/Source/Tiles/WheelmapTileManager.cs
+++ Wheelmap.Shared/Source/Tiles/WheelmapTileManager.cs
@@ -10,13 +10,12 @@ namespace Wheelmap.Tiles {
             updater.EnableNotificationQueue(true);
             updater.Clear();
             
-            updater.Update(createNotification());
-            updater.Update(createNotification());
-            updater.Update(createNotification());
+            updater.Update(createAppIconTiles());
+            updater.Update(createSecondaryIconTiles());
 
         }
 
-        private static TileNotification createNotification() {
+        private static TileNotification createAppIconTiles() {
 
             var square150x150 = TileContentFactory.CreateTileSquare150x150Image();
             square150x150.Image.Src = "ms-appx:///Assets/App/square_150x150/square.png";
@@ -27,6 +26,22 @@ namespace Wheelmap.Tiles {
 
             var tileContent = TileContentFactory.CreateTileSquare310x310Image();
             tileContent.Image.Src = "ms-appx:///Assets/App/square_150x150/square.png";
+            tileContent.Wide310x150Content = wide310x150;
+
+            return tileContent.CreateNotification();
+        }
+
+        private static TileNotification createSecondaryIconTiles() {
+
+            var square150x150 = TileContentFactory.CreateTileSquare150x150Image();
+            square150x150.Image.Src = "ms-appx:///Assets/Tiles/square_150x150/square.png";
+
+            var wide310x150 = TileContentFactory.CreateTileWide310x150Image();
+            wide310x150.Image.Src = "ms-appx:///Assets/Tiles/wide_310x150/wide.png";
+            wide310x150.Square150x150Content = square150x150;
+
+            var tileContent = TileContentFactory.CreateTileSquare310x310Image();
+            tileContent.Image.Src = "ms-appx:///Assets/Tiles/square_150x150/square.png";
             tileContent.Wide310x150Content = wide310x150;
 
             return tileContent.CreateNotification();
